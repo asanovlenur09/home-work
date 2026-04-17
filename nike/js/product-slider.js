b18eb3f9d5bd2ebd9d51911f3980cb67b100354d
asanovlenur09 <asanovlenur09@gmail.com>
2026-04-17 16:11:26 +0300

    nike-130

---
+++ nike/js/product-slider.js
@@ -0,0 +1,17 @@
+export const productSlider = () => {
+  new Swiper('.product__slider', {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    loop: true,
+    mousewheel: {
+    forceToAxis:true
+    },
+    navigation: {
+      prevEl: ".product__slider-button--prev"
+    },
+    navigation: {
+      prevEl: ".product__slider-button--prev",
+      nextEl: ".product__slider-button--next"
+    }
+  })
+}
